081b4ee06ff30153220e41d6e29f5fef2572306f
Kulraj Dhaliwal <kulrajd@uci.edu>
2019-02-01 14:09:31 -0800

    Added login page and updated index page

---
+++ project1/WebContent/movielist.js
@@ -0,0 +1,52 @@
+function handleMovieListResult(resultData) {
+	createMovieCard(resultData);
+}
+
+function createMovieCard(data) {
+	var movieCardCollection = jQuery("#movie_card_collection");
+	
+	for(var i = 0; i < Math.min(20, data.length); ++i)
+	{
+		var rowHTML =  '<div class="col-md-6 col-lg-4 mb-3">';
+		rowHTML += "<div class=\"card bg-light\">";
+		rowHTML += "<div class=\"card-body\">";
+		rowHTML += "<h5 class=\"card-title\">" + '<a href="single-movie.html?id=' + 
+			data[i]["movie_id"] + '">' + data[i]["movie_title"] + "</a>" +
+			" (" + data[i]["movie_year"] + ")" + "</h5>";
+		rowHTML += '<p class="card-text mb-1"><strong>Director</strong>: ' + 
+			data[i]["movie_director"] + "</p>";
+
+		rowHTML += '<p class="card-text mb-1"><strong>Rating</strong>: ' + 
+			'<i class="far fa-star"></i> ' + data[i]["movie_rating"] + "</p>";
+
+		rowHTML += '<p class="card-text mb-1"><strong>Genre(s)</strong>: '
+		data[i]["movie_genres"].forEach(function(genre){
+			rowHTML += genre + ', ';
+		});
+		rowHTML = rowHTML.slice(0, rowHTML.lastIndexOf(",")) + "</p>";
+		
+		rowHTML += '<p class="card-text mb-1"><strong>Stars</strong>: ';
+		data[i]["movie_stars"].forEach(function(star){
+			rowHTML += '<a href="single-star.html?id=' + star["star_id"] + '">' + star["star_name"] + "</a>";
+			rowHTML += ', ';
+		});
+		rowHTML = rowHTML.slice(0, rowHTML.lastIndexOf(",")) + "</p>";
+		
+		rowHTML += '<button type="button" class="btn btn-success mt-2">Add to cart</button>'
+		
+		rowHTML += "</div></div></div>";
+		movieCardCollection.append(rowHTML);
+	}
+	
+}
+
+jQuery.ajax({
+    dataType: "json", // Setting return data type
+    method: "GET", // Setting request method
+    url: "api/movies", // Setting request url, which is mapped by StarsServlet in Stars.java
+    success: (resultData) => handleMovieListResult(resultData) // Setting callback function to handle data returned successfully by the StarsServlet
+});
+
+
+
+
---
+++ project1/src/IndexServlet.java
@@ -0,0 +1,34 @@
+import com.google.gson.JsonObject;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ * This IndexServlet is declared in the web annotation below, 
+ * which is mapped to the URL pattern /api/index.
+ */
+@WebServlet(name = "IndexServlet", urlPatterns = "/api/index")
+public class IndexServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * handles POST requests to store session information
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        HttpSession session = request.getSession();
+        String sessionId = session.getId();
+        Long lastAccessTime = session.getLastAccessedTime();
+
+        JsonObject responseJsonObject = new JsonObject();
+        responseJsonObject.addProperty("sessionID", sessionId);
+        responseJsonObject.addProperty("lastAccessTime", new Date(lastAccessTime).toString());
+
+        // write all the data into the jsonObject
+        response.getWriter().write(responseJsonObject.toString());
+    }
+}
